cbe0ff367ade958796b41c7e947ba74da407f980
Yásmin Monterroso <yasminmonterrosoe@gmail.com>
2023-06-27 01:10:07 -0600

    Finalizado version 1

--- IA1jun23/R201801385.java
+++ IA1jun23/R201801385.java
@@ -1,56 +1,96 @@
 package IA1jun23;
+
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
 /**
  * R201801385 - a robot by (your name here)
  */
-public class R201801385 extends Robot
-{
-	/**
-	 * run: R201801385's default behavior
-	 */
+public class R201801385 extends AdvancedRobot {
+
+	private int direccion = 1;
+
+	private boolean atacando = false;
+	private double distanciaAtk = 0;
+
 	public void run() {
-		// Initialization of the robot should be put here
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		setBodyColor(Color.pink);
+		setGunColor(Color.pink);
+		setRadarColor(Color.pink);
+		setBulletColor(Color.pink);
+		setScanColor(Color.pink);
 
 		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(50);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+		while (true) {
+
+			// ahead(50);
+			// turnGunRight(360);
+			// back(100);
+			// turnGunRight(360);
+
+			tatakaeee();
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
+		stop();
 		fire(1);
+
+		distanciaAtk = e.getDistance();
+		setTurnRight(e.getBearing()); // me volteo pa donde esta el otro robot
+		atacando = true;
 	}
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
-		back(10);
+
+		double anguloBala = 0;
+
+		if (e.getHeading() >= 0 && e.getHeading() < 180) {
+			anguloBala = -90;
+		} else {
+			anguloBala = 90;
+		}
+
+		setTurnRight(90);
+		setAhead(-150);
+		waitFor(new TurnCompleteCondition(this));
+
 	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
+
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+
+		revertirDireccion();
+	}
+
+	public void onHitRobot(HitRobotEvent e) {
+		revertirDireccion();
+	}
+
+	public void revertirDireccion() {
+		// si hay un muro, avanzo en la direccion contraria
+		direccion = direccion * -1;
+	}
+
+	// MIS METODOS
+
+	public void tatakaeee() {
+		// metodo default para ir a la pelea (el titulo es una referencia a SNK)
+
+		// si no estoy atacando a alguien, ando escaneando a quien atacar
+
+		if (atacando) {
+			setAhead(distanciaAtk - 150);
+			atacando = false;
+		} else {
+			setTurnGunRight(90);
+			setAhead(1500 * direccion);
+		}
+
+		waitFor(new TurnCompleteCondition(this));
+
+	}
 }
